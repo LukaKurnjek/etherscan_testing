b5f1498801d896e3ca1409447ff9d181e0fef303
Luka Kurnjek <luka.kurnjek@outlook.com>
2022-05-22 14:44:56 +0200

    Changing tabs to spaces in test10.js.

--- code/test_10/test10.js
+++ code/test_10/test10.js
@@ -5,17 +5,17 @@ fixture`Starting test 10.`
     .page`https://etherscan.io/register`;
 
 test('Test 10', async t => {
-	// Function that gets the full URL path from the current window
-	const getPageUrl = ClientFunction(() => window.location.href);	
+    // Function that gets the full URL path from the current window
+    const getPageUrl = ClientFunction(() => window.location.href);	
 	
     await t
-		// Click on the button to accept cookies
-		.click('#btnCookie.btn.btn-sm.btn-primary.text-nowrap.py-1')
+        // Click on the button to accept cookies
+        .click('#btnCookie.btn.btn-sm.btn-primary.text-nowrap.py-1')
 		
-		// Enter username
-		.typeText('#ContentPlaceHolder1_txtUserName.form-control.form-control-sm', 'newtestuser')
-		
-		// Enter email address twice
+        // Enter username
+        .typeText('#ContentPlaceHolder1_txtUserName.form-control.form-control-sm', 'newtestuser')
+    
+        // Enter email address twice
         .typeText('#ContentPlaceHolder1_txtEmail.form-control.form-control-sm', 'my_test_email_etherscan@gmail.com')
         .typeText('#ContentPlaceHolder1_txtConfirmEmail.form-control.form-control-sm', 'my_test_email_etherscan@gmail.com')
         
@@ -41,19 +41,19 @@ test('Test 10', async t => {
         .switchToMainWindow()
         .switchToIframe(Selector('iframe').withAttribute('title','recaptcha challenge expires in two minutes'));
         
-	// Get the donwload link from the audio download buttun
-	const downloadLink = await (Selector('.rc-audiochallenge-tdownload-link').withAttribute('title','Alternatively, download audio as MP3')()).getAttribute('href');
+    // Get the donwload link from the audio download buttun
+    const downloadLink = await (Selector('.rc-audiochallenge-tdownload-link').withAttribute('title','Alternatively, download audio as MP3')()).getAttribute('href');
 
-	// Execute the python script for downloading, converting and parsing the audio file
-	const execSync = require('child_process').execSync;
-	const output = execSync('python process_mp3.py ' + '\"' + downloadLink + '\"').toString();
-	// It is neccessary to wrap the downloadLink into "" quotation marks because it contains an ampersand symbol
-			
-	await t
-		// Type in the audio text and click on the verify button
-		.wait(5000) // if you are to fast google gives you another task
-		.typeText('#audio-response.rc-response-input-field.label-input-label', output)
-		.click('#recaptcha-verify-button.rc-button-default.goog-inline-block')
+    // Execute the python script for downloading, converting and parsing the audio file
+    const execSync = require('child_process').execSync;
+    const output = execSync('python process_mp3.py ' + '\"' + downloadLink + '\"').toString();
+    // It is neccessary to wrap the downloadLink into "" quotation marks because it contains an ampersand symbol
+            
+    await t
+        // Type in the audio text and click on the verify button
+        .wait(5000) // if you are to fast google gives you another task
+        .typeText('#audio-response.rc-response-input-field.label-input-label', output)
+        .click('#recaptcha-verify-button.rc-button-default.goog-inline-block')
         // Click on the "Create An Account" button
         .switchToMainWindow() // switches back to main window
         .click('#ContentPlaceHolder1_btnRegister.btn.btn-sm.btn-primary');
